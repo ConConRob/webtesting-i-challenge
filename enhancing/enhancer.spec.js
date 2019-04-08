a8e0334a4555026b9bafd7de8f0c3b7d6a2bfc68
conconrob <connortprobertson@gmail.com>
2019-04-08 13:02:28 +0100

    done and tested succeed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,7 +10,16 @@ const weapon = {
 describe("Weapon enhancers", () => {
   describe("repair a weapon", () => {
     it("weapon is repaired 100%", () => {
-      expect(enhancer.repair(weapon)).toEqual({ ...weapon, durability: 100 })
+      expect(enhancer.repair(weapon)).toEqual({ ...weapon, durability: 100 });
+    })
+  })
+
+  describe("Succeed at enhancing weapon", () => {
+    it("weapon enhanced when bellow 20", () => {
+      expect(enhancer.succeed(weapon)).toEqual({ ...weapon, enhancement: weapon.enhancement + 1 });
+    })
+    it("weapon already at max enhancement",() => {
+      expect(enhancer.succeed({ ...weapon, enhancement: 20 })).toEqual({ ...weapon, enhancement: 20 })
     })
   })
 })
